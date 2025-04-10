1aaa15ec473187c85abf4b337ca6b7d33c9afc3f
AntoCreed777 <abenavides@desem.cl>
2025-04-10 18:42:35 -0400

    feat: Codigo inicial que recibe el input

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
+#include <vector>
+
+#define rep(i,n) for(int i=0;i<(int)n;++i)
+#define repx(i,x,n) for(int i=x;i<(int)n;++i)
+#define invrep(i,n) for(int i=n;i>=0;i--)
+#define pb push_back
+
+void FunctionOptimization() {
+   std::ios_base::sync_with_stdio(false);
+   std::cin.tie(0);
+}
 
 int main() {
+    FunctionOptimization();
+
+    int vida, numero_enemigos;
+
+    std::cin >> vida >> numero_enemigos;
+
+    std::vector<int> vida_enemigos(numero_enemigos);
+    std::vector<int> ataque_enemigos(numero_enemigos);
+    std::vector<bool> super_enemigos(numero_enemigos);  // En el enunciado se les denomina CANO
+
+    rep(i,numero_enemigos) std::cin >> vida_enemigos[i];
+    rep(i,numero_enemigos) std::cin >> ataque_enemigos[i];
+    rep(i,numero_enemigos) {    //Esto es porque me da error de formato el std::cin con el vector<bool>
+        int aux;
+        std::cin >> aux;
+        if (aux) super_enemigos[i] = true;
+        else super_enemigos[i] = false;
+    }
+
+
+
+
     std::cout << "Hello World" << std::endl;
 
     return 0;
