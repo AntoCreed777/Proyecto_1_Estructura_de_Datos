0df2a883270e8ccf8c16961f5f20da6cd4e82536
AntoCreed777 <abenavides@desem.cl>
2025-04-11 14:19:17 -0400

    fix: Correcion de logica en codigo sin Cola

--- Sin_Cola/main.cpp
+++ Sin_Cola/main.cpp
@@ -34,33 +34,41 @@ int main() {
 
     int damage_acumulado = 0;
     int damage_mechon = 2;
-    int acumulador_golpes_mechon = 0;
+    int acumulador_enemigos_eliminados_mechon = 0;
     int acumulador_golpes_super_enemigo = 0;
 
     bool dividido = false;
 
     rep(i,numero_enemigos) {
         while(vida_enemigos[i] > 0) {
+            //Upgrade damage Mechon
+            if(acumulador_enemigos_eliminados_mechon == 5) {
+                acumulador_enemigos_eliminados_mechon = 0;
+                damage_mechon++;
+                std::cout << "aumenta el damage mechon" << std::endl;
+            }
+
             int aux_vida = vida_enemigos[i];
             vida_enemigos[i] -= damage_mechon;  //Enemigo recibe damage
 
             vida_mechon -= ataque_enemigos[i];  //El mechon recibe damage
 
-            acumulador_golpes_mechon++;
             acumulador_golpes_super_enemigo++;
-
-            //Upgrade damage Mechon
-            if(acumulador_golpes_mechon == 5) {
-                acumulador_golpes_mechon = 0;
-                damage_mechon++;
-            }
+            
+            std::cout << "vida mechon: " << vida_mechon << std::endl;
             
             //Aumentar el damage acumulado realizado
             if(vida_enemigos[i] <= 0) {
+                acumulador_enemigos_eliminados_mechon++;
                 damage_acumulado+=aux_vida;
+                std::cout << "damage acumulado m: " << damage_acumulado << std::endl;
+
                 break;
             }
-            else damage_acumulado += damage_mechon;
+            else {
+                damage_acumulado += damage_mechon;
+                std::cout << "damage acumulado v: " << damage_acumulado << std::endl;
+            }
 
             //SI es un super enemigo y puede cumple requisito para dividirse
             if(super_enemigos[i] && acumulador_golpes_super_enemigo == 2) {
@@ -72,32 +80,38 @@ int main() {
             if(vida_mechon <= 0) break;
         }
 
+        std::cout << "enemigo: " << i << std::endl; 
+
         acumulador_golpes_super_enemigo = 0;
 
         //Si se murio el mechon, finaliza la pelea
         if(vida_mechon <= 0) break;
 
         if(dividido) {
+
+            std::cout << std::endl << "se dividio" << std::endl;
             //Los 2 enemigos generados de la divicion de un super enemigo
             int enemigo1[2] = {vida_enemigos[i]-1,ataque_enemigos[i]-1};
             int enemigo2[2] = {vida_enemigos[i]-1,ataque_enemigos[i]-1};
 
             //Pelea con el primer enemigo
             while (true) {
+                //Upgrade damage Mechon
+                if(acumulador_enemigos_eliminados_mechon == 5) {
+                    acumulador_enemigos_eliminados_mechon = 0;
+                    damage_mechon++;
+                }
+
                 int aux_vida = enemigo1[0];
                 enemigo1[0] -= damage_mechon;   //Enemigo recibe damage
     
                 vida_mechon -= enemigo1[1];     //Mechon recibe damage
 
-                //Upgrade damage Mechon
-                if(acumulador_golpes_mechon == 5) {
-                    acumulador_golpes_mechon = 0;
-                    damage_mechon++;
-                }
     
                 //Aumentar el damage acumulado realizado
                 if(enemigo1[0] <= 0) {
                     damage_acumulado+=aux_vida;
+                    acumulador_enemigos_eliminados_mechon++;
                     break;
                 }
                 else damage_acumulado += damage_mechon;
@@ -111,20 +125,22 @@ int main() {
 
             //Pelea con el segundo enemigo
             while (true) {
+                //Upgrade damage Mechon
+                if(acumulador_enemigos_eliminados_mechon == 5) {
+                    acumulador_enemigos_eliminados_mechon = 0;
+                    damage_mechon++;
+                }
+
                 int aux_vida = enemigo2[0];
                 enemigo2[0] -= damage_mechon;   //Enemigo recibe damage
     
                 vida_mechon -= enemigo2[1];     //Mechon recibe damage
 
-                //Upgrade damage Mechon
-                if(acumulador_golpes_mechon == 5) {
-                    acumulador_golpes_mechon = 0;
-                    damage_mechon++;
-                }
     
                 //Aumentar el damage acumulado realizado
                 if(enemigo2[0] <= 0) {
                     damage_acumulado+=aux_vida;
+                    acumulador_enemigos_eliminados_mechon++;
                     break;
                 }
                 else damage_acumulado += damage_mechon;
@@ -138,7 +154,10 @@ int main() {
         //Si se murio el mechon, finaliza la pelea
         if(vida_mechon <= 0) break;
 
+        std::cout << std::endl;
     }
+
+    std::cout << std::endl;
 
     std::cout << damage_acumulado << std::endl;
 
